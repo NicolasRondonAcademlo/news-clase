49d7365211bc45998aa45776037f2faa2a6ddf6b
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-01-24 22:26:04 -0500

    fix

--- articles/permissions.py
+++ articles/permissions.py
@@ -1,6 +1,4 @@
-from math import perm
 from rest_framework import permissions
-
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
--- articles/views.py
+++ articles/views.py
@@ -3,9 +3,9 @@ from rest_framework import viewsets
 # Create your views here.
 from .models import Article
 from .serializers import ArticleSerializer
-from .permissions import IsOwnerOrReadOnly, IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = (IsOwnerOrReadOnly)
+    permission_classes = (IsOwnerOrReadOnly,)
